abd4502195b2cf3bf8b954e8b54337a3b532b5a1
Fortunejite <111015282+Fortunejite@users.noreply.github.com>
2024-11-22 15:56:42 +0100

    Update conf.ts

--- appwrite/conf.ts
+++ appwrite/conf.ts
@@ -4,10 +4,10 @@ export const config = {
   endpoint: process.env.NEXT_PUBLIC_APPWRITE_URL!,
   projectId: process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID!,
   databaseId: process.env.NEXT_PUBLIC_APPWRITE_DATABASE_ID!,
-  userCollectionId: process.env.NEXT_PUBLIC_APPWRITE_USER_COLLECTION_ID!,
-  appoinmentsCollectionId: process.env.NEXT_PUBLIC_APPWRITE_APPOINTMENTS_COLLECTION_ID!,
+  userCollectionId: '670a248800049761218e',
+  appoinmentsCollectionId: '670a254100339e546aa4',
   medicationCollectionId: process.env.NEXT_PUBLIC_APPWRITE_MEDICATIONS_COLLECTION_ID!,
-  doctorsCollectionId: process.env.NEXT_PUBLIC_APPWRITE_DOCTORS_COLLECTION_ID!,
+  doctorsCollectionId: '673dfed1003665290768',
   storageId: process.env.NEXT_PUBLIC_APPWRITE_STORAGE_ID!,
   avatarId : process.env.NEXT_PUBLIC_APPWRITE_AVATAR_ID!,
 };
