aeb9c9436b9471e5c9df572c2bfb00c63a34ba27
CHEGEBB <chegephil24@gmail.com>
2024-12-20 16:11:36 +0300

    Refactor navbar component to update logo image and styling

--- app/page.tsx
+++ app/page.tsx
@@ -130,14 +130,19 @@ const LandingPage = () => {
       {/* Navigation */}
       <nav className={`fixed w-full z-50 transition-all duration-300 ${isScrolled ? 'bg-slate-900/80 backdrop-blur-md' : 'bg-transparent'}`}>
         <div className="container flex items-center justify-between px-4 py-4 mx-auto sm:px-6">
-          <Link href="/" className="flex items-center">
-            <Image 
-              src="/assets/icons/logo.png" 
-              width={80} 
-              height={80}
-              alt="HealthMaster logo" 
-              className="w-8 h-8 mr-2 sm:w-10 sm:h-10" 
-            />
+          <Link href="/" className="flex items-center gap-4">
+          <Image
+            src="/assets/icons/1.png"
+            width={150}
+            height={150}
+            alt="HealthMaster logo"
+            className="rounded-full border border-slate-400 p-1
+            w-10 h-10
+             sm:w-10 sm:h-10 
+             md:w-12 md:h-12 
+             lg:w-16 lg:h-16
+             hover:scale-110 transition-transform duration-300 ease-in-out"
+          />
             <span className="text-lg font-bold sm:text-xl font-sora text-emerald-500">
               Health Master
             </span>
