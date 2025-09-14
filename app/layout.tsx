516b404ca767384a9d2e4c7af4d9165485e80921
@BrianChege <123733116+CHEGEBB@users.noreply.github.com>
2025-09-14 21:53:38 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,31 +12,35 @@ const geistMono = localFont({
   variable: "--font-geist-mono",
   weight: "100 900",
 });
+
 export const metadata: Metadata = {
   title: "Healthmaster",
   description: "HealthMaster helps you keep track of your health.",
   icons: {
-    icon: "/adaptive.png", 
-    apple: "/adaptive.png", 
+    icon: "/adaptive.png",
+    apple: "/adaptive.png",
     other: [
       {
         rel: "icon",
-        url: "/adaptive.png", 
+        url: "/adaptive.png",
         sizes: "32x32",
       },
       {
         rel: "icon",
-        url: "/adaptive.png", 
+        url: "/adaptive.png",
         sizes: "16x16",
       },
       {
         rel: "icon",
-        url: "/adaptive.png", 
-        sizes: "180x180",  // For Apple devices
+        url: "/adaptive.png",
+        sizes: "180x180", // For Apple devices
       },
     ],
   },
-};  
+  verification: {
+    google: "Wm-19FiyRyX8MdtXQwt7JBRCxf3PGXSNFP74xgNCpOk",
+  },
+};
 
 export default function RootLayout({
   children,
